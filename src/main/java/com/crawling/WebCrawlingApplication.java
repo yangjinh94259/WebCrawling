6119d40a2046f75f1a615a9c903c8527877c23c3
yangjinh94259 <yangjinh94259@gmail.com>
2023-07-19 13:05:35 +0900

    WebCrawling

---
+++ src/main/java/com/crawling/WebCrawlingApplication.java
@@ -0,0 +1,13 @@
+package com.crawling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCrawlingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCrawlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crawling/WebCrawlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crawling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCrawlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
